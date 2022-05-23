89135414a0c461515c413a7877d39c58905077b0
Juulia Santala <jusantal@cisco.com>
2022-05-23 19:56:52 +0300

    pyats initial scripts

---
+++ change_verification/change_verification.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pyATS sample functions for taking a snapshot of the network and comparing two
+snapshots to find the differences..
+
+Copyright (c) 2022 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+from pyats import topology
+from genie.utils.diff import Diff
+
+__copyright__ = "Copyright (c) 2022 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+def get_snapshot(testbed_file, uut, feature):
+    '''
+    Function that returns a snapshot of the selected feature.
+    '''
+    testbed = topology.loader.load(testbed_file) # load the testbed
+    device = testbed.devices[uut] # find device with hostname / alias
+    device.connect(log_stdout=True) # connect to the device
+    snapshot = device.learn(feature) # learn a selected feature such as VLAN
+    device.disconnect()
+    return snapshot
+
+def find_difference(snapshot1, snapshot2):
+    '''
+    Function to compare two snapshots and find the possible differences.
+    '''
+    difference = Diff(snapshot1, snapshot2) # Define the snapshots to compare
+    difference.findDiff() # Find differences between the snapshots defined
+    return difference
+
+if __name__ == '__main__':
+    pre_snapshot = get_snapshot("testbed.yaml", "uut", "interface")
+    input("are you ready? ")
+    post_snapshot = get_snapshot("testbed.yaml", "uut", "interface")
+
+    diff = find_difference(pre_snapshot, post_snapshot)
+    print(diff)
+
