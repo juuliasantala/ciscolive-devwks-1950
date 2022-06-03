69d26fe36114494a59aa543c3b81885bdf01afa9
Juulia Santala <jusantal@cisco.com>
2022-06-03 16:42:28 +0300

    setup file adjusted, solution dynamic and static deploy files renamed

--- setup.sh
+++ setup.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+echo "Connecting to VPN..."
 startvpn.sh &
+echo "Connected!"
 
 mkdir -p /home/developer/.ssh
 echo "Host *
     KexAlgorithms +diffie-hellman-group14-sha1" > /home/developer/.ssh/config
 
-echo "Installing requirements..."
-pip install pyats[full]==22.4 -q
-echo "Done!\n"
-
+echo " "
 echo "Verifying..."
 echo "Connecting to Webex: "
 curl -s -o /dev/null -w "%{http_code}" https://webexapis.com/v1/rooms/$WEBEX_ROOM -H "Accept: application/json" -H "Authorization: Bearer $WEBEX_TOKEN"
 
-echo "\nConnecting to switch using RESTCONF: "
+echo " "
+echo "Connecting to CSR using RESTCONF: "
 curl -s -o /dev/null -w "%{http_code}" -k https://$CSR_IP/restconf/ -u "$CSR_USERNAME:$CSR_PASSWORD"
 
-echo "\nverifying pyATS:"
-pyats version check
+echo " "
+echo "Connecting to CSR using pyATS to retrieve the platform details: "
+pyats parse "show platform" --testbed-file ~/src/ciscolive-devwks-1950/solutions/change_verification/testbed.yaml
 
-echo "\nSetup ready!"
+echo " "
+echo "Setup ready!"
---
+++ solutions/configuration/static_deploy.py
@@ -0,0 +1,73 @@
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+def edit_interfaces(ip, username, password, config):
+    url = f"https://{ip}:443/restconf/data/ietf-interfaces:interfaces"
+    headers = {
+        'Content-Type': 'application/yang-data+json',
+        'Accept': 'application/yang-data+json',
+    }
+    auth = (username, password)
+
+    response = requests.put(url, headers=headers, auth=auth, data=config, verify=False)
+    print(f"Status code of deploying the configuration change: {response.status_code}")
+
+if __name__ == "__main__":
+
+    # DEVICE DETAIL
+    csr_ip = "10.10.20.48"
+    csr_user = "developer"
+    csr_password = "C1sco12345"
+
+    # CONFIGURATION
+    interface_config = '''
+    {
+        "ietf-interfaces:interfaces": {
+            "interface": [
+                {
+                    "name": "GigabitEthernet1",
+                    "description": "MANAGEMENT INTERFACE - DON'T TOUCH ME",
+                    "type": "iana-if-type:ethernetCsmacd",
+                    "enabled": true,
+                    "ietf-ip:ipv4": {
+                        "address": [
+                            {
+                                "ip": "10.10.20.48",
+                                "netmask": "255.255.255.0"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "name": "GigabitEthernet2",
+                    "description": "Network Interface",
+                    "type": "iana-if-type:ethernetCsmacd",
+                    "enabled": false
+                },
+                {
+                    "name": "GigabitEthernet3",
+                    "description": "Network Interface",
+                    "type": "iana-if-type:ethernetCsmacd",
+                    "enabled": false
+                },
+                {
+                    "name": "Loopback1",
+                    "description": "Configured from Python",
+                    "type": "iana-if-type:softwareLoopback",
+                    "enabled": true,
+                    "ietf-ip:ipv4": {
+                        "address": [
+                            {
+                                "ip": "10.20.50.20",
+                                "netmask": "255.255.255.0"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }'''
+
+    edit_interfaces(csr_ip, csr_user, csr_password, interface_config)
