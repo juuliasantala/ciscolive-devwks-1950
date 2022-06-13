25c75f75f43f5258a01fe75063f612748fe59f9b
Juulia Santala <jusantal@cisco.com>
2022-06-13 08:54:43 -0700

    tweaking setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 echo "Connecting to VPN..."
 startvpn.sh &
-echo "Connected!"
 
 mkdir -p /home/developer/.ssh
 echo "Host *
@@ -13,10 +12,6 @@ echo " "
 echo "Verifying..."
 echo "Connecting to Webex: "
 curl -s -o /dev/null -w "%{http_code}" https://webexapis.com/v1/rooms/$WEBEX_ROOM -H "Accept: application/json" -H "Authorization: Bearer $WEBEX_TOKEN"
-
-echo " "
-echo "Connecting to CSR using RESTCONF: "
-curl -s -o /dev/null -w "%{http_code}" -k https://$CSR_IP/restconf/ -u "$CSR_USERNAME:$CSR_PASSWORD"
 
 echo " "
 echo "Connecting to CSR using pyATS to retrieve the platform details: "
