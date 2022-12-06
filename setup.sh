3b2d33b129f1f3cdd928c9f6034fc092807d719f
Juulia Santala <jusantal@cisco.com>
2022-12-06 22:57:00 +1100

    fixing 8 and 9

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ echo "GET READY FOR YOUR CISCO LIVE WORKSHOP EXPERIENCE :)"
 echo -n "What is your pod number? "
 read POD_NUMBER
 
-export POD_NUMBER=`printf "%02d" "${POD_NUMBER}"` 
-export DEVICE_IP="64.102.247.2"`printf "%02d" "${POD_NUMBER}"` 
+POD_NUMBER=$(echo ${POD_NUMBER} | sed 's/^0*//')
+
+export POD_NUMBER=$(printf "%02d" "${POD_NUMBER}")
+export DEVICE_IP="64.102.247.2${POD_NUMBER}"
 
 python ./setup_test.py
